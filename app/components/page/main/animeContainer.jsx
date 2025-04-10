fe0c43f4b3bb6868225e5690ffe654513a65a473
N4chtF4ust <ronguinto57@gmail.com>
2025-04-10 09:03:27 -0100

    take two

--- app/components/page/main/animeContainer.jsx
+++ app/components/page/main/animeContainer.jsx
@@ -1,31 +1,32 @@
+'use client';
 
-import React from 'react'
+import React, { Suspense } from 'react';
 import LoadingSpinner from "@/app/components/loading/loading";
 import Pagination from "@/app/components/pagination/pagination";
-import  AnimeCard  from "@/app/components/page/main/animeCard";
+import AnimeCard from "@/app/components/page/main/animeCard";
 
-function AnimeContainer({loading,animeList,page}) {
+function AnimeContainer({ loading, animeList, page }) {
   return (
     <>
+      <section className={loading ? "" : "anime-container"}>
+        {loading ? (
+          <div className="relative h-[50svh] grid place-content-center">
+            <LoadingSpinner LoadingColor="#FFFFFF" strokeColor="#FFFFFF" />
+          </div>
+        ) : (
+          animeList.results.map((anime, index) => (
+            <AnimeCard key={anime.id} anime={anime} index={index} />
+          ))
+        )}
+      </section>
 
-           <section className={loading ? "" : "anime-container"}>
-               {loading ? (
-                 <div className="relative h-[50svh]  grid place-content-center">
-                      <LoadingSpinner LoadingColor="#FFFFFF" strokeColor="#FFFFFF" />
-                 </div>
-                 ) : (
-                 animeList.results.map((anime,index) => 
-                     ( <AnimeCard key={anime.id} anime={anime} index={index}/> )
-                   )
-                 )
-               }
-             </section>
-   
-             { loading ? null : (<Pagination   currentPage={page} totalPages={animeList.totalPages} />) }
-          
-    </> 
-          
-  )
+      {!loading && (
+        <Suspense fallback={<div>Loading pagination...</div>}>
+          <Pagination currentPage={page} totalPages={animeList.totalPages} />
+        </Suspense>
+      )}
+    </>
+  );
 }
 
-export default AnimeContainer
+export default AnimeContainer;
